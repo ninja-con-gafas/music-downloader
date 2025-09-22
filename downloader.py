6a8115b4b328884c17c47e0e368c9e80228780ba
ninja-con-gafas <absdarekar01@gmail.com>
2025-09-22 21:47:47 +0530

    feat: add YouTube download functionality feat: add CSV extraction for YouTube URLs feat: integrate YouTube workflow into the Streamlit application
    - Introduce new functions in `downloader.py` to support downloading audio from YouTube:
    - `download_youtube`: processes YouTube URLs, retrieves metadata, and downloads audio concurrently.
    - `extract_youtube_urls`: extracts and deduplicates YouTube URLs from CSV files.
    - Replace text area input with CSV upload for URLs.
    - Display download statistics (total, successful, failed).
    - Integrate new YouTube functions with session state handling.

--- downloader.py
+++ downloader.py
@@ -1,5 +1,6 @@
+import re
 from concurrent import futures
-from google.youtube import download_audio_as_mp3, get_video_id, search_youtube
+from google.youtube import download_audio_as_mp3, get_video_id, get_video_metadata, search_youtube
 from logging import basicConfig, getLogger, INFO
 from os import listdir, makedirs, path
 from pandas import DataFrame, read_csv
@@ -64,6 +65,63 @@ def download_shazams(shazams: DataFrame) -> None:
         raise
 
 @StreamlitLogHandler.decorate
+def download_youtube(urls: DataFrame) -> None:
+    """
+    Process YouTube URLs and initiate downloads concurrently.
+
+    Parameters:
+        urls (DataFrame): DataFrame containing YouTube video URLs.
+    """
+
+    logger.info("Starting YouTube download process.")
+
+    try:
+        urls = (urls.assign(video_id=lambda x: x['url'].apply(get_video_id))
+        .drop_duplicates(subset=['video_id'])
+        .assign(
+            metadata=lambda x: x['video_id'].apply(get_video_metadata),
+            name=lambda x: x.apply(
+                lambda row: re.sub(
+                    r'[^a-zA-Z0-9]',
+                    ' ',
+                    f"{row['metadata'].get('title')} {row['metadata'].get('author_name')}")
+                            + f" {row['video_id']}",
+                axis=1)))
+
+        logger.info("Starting concurrent downloads of audio streams.")
+        ctx = get_script_run_ctx()
+
+        with futures.ThreadPoolExecutor() as executor:
+            tasks = [
+                executor.submit(
+                    download_audio_as_mp3,
+                    download_path=DOWNLOADS_PATH,
+                    file_name=row["name"],
+                    url=row["url"]
+                ) for row in urls.to_dict(orient="records")]
+
+            for thread in executor._threads:
+                try:
+                    add_script_run_ctx(thread, ctx)
+                except Exception as e:
+                    logger.exception(f"Failed to add_script_run_ctx to thread {thread.name}: {e}")
+
+            for future in futures.as_completed(tasks):
+                try:
+                    future.result()
+                except Exception as e:
+                    logger.exception(f"Error in download task: {e}")
+
+        logger.info("Marking downloaded tracks and storing report in session state.")
+        session_state.report = (
+            urls.assign(is_downloaded=lambda x: x["video_id"].apply(is_audio_downloaded)))
+
+        logger.info("YouTube download process completed successfully.")
+    except Exception as e:
+        logger.error(f"Error during YouTube download process: {str(e)}")
+        raise
+
+@StreamlitLogHandler.decorate
 def extract_shazams(file_path: str) -> DataFrame:
     """
     Extract unique Shazam tracks from a CSV file, dropping unnecessary columns.
@@ -88,6 +146,28 @@ def extract_shazams(file_path: str) -> DataFrame:
     except Exception as e:
         logger.error(f"Failed to extract Shazam data: {str(e)}")
         raise Exception(f"Failed to extract Shazam data: {str(e)}")
+    
+@StreamlitLogHandler.decorate
+def extract_youtube_urls(file_path: str) -> DataFrame:
+    """
+    Extract unique YouTube URLs from a CSV file.
+
+    Parameters:
+        file_path (str): Path to the CSV file containing YouTube URLs.
+
+    Returns:
+        DataFrame: A DataFrame containing the unique YouTube URLs.
+    """
+
+    logger.info(f"Extracting YouTube URLs from: {file_path}")
+
+    try:
+        return read_csv(filepath_or_buffer=file_path) \
+            .drop_duplicates(subset=["url"]) \
+            .sort_values(by=["url"])
+    except Exception as e:
+        logger.error(f"Failed to extract YouTube URLs: {str(e)}")
+        raise Exception(f"Failed to extract YouTube URLs: {str(e)}")
 
 @StreamlitLogHandler.decorate
 def is_audio_downloaded(video_id: str) -> bool:
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ A web-based application to download song identified by Shazam or from YouTube wi
 
 from datetime import datetime
 from pandas import DataFrame
-from downloader import extract_shazams, download_shazams
+from downloader import extract_shazams, extract_youtube_urls, download_shazams, download_youtube
 from streamlit import (dataframe, error, file_uploader, markdown, set_page_config, session_state, sidebar, success, 
                        tabs, text, text_area)
 from typing import List
@@ -59,13 +59,33 @@ with tab_shazam:
 with tab_youtube:
     markdown(
         """
-        Paste one or more YouTube video URLs, each on a separate line.
+        1. Prepare a CSV file with a column named `url` containing YouTube or YouTube Music video URLs.
+        2. Upload the CSV file below.
         """,
         unsafe_allow_html=True)
 
-    urls: str = text_area(
-        "Paste YouTube video URLs",
-        height=150)
+    uploaded_csv = file_uploader(
+        "Upload your CSV file containing YouTube or YouTube Music videos URLs.",
+        type="csv",
+        help="The CSV file must contain a column named `url` with the video URLs.")
+    
+    if uploaded_csv is not None:
+        try:
+            session_state.youtube_urls = extract_youtube_urls(uploaded_csv)
+            success(f"Successfully loaded {len(session_state.youtube_urls)} unique URLs from the given list.")
+            
+            download_youtube(session_state.youtube_urls)
+
+            sidebar.markdown("Download Statistics")
+            total_downloads: int = len(session_state.report)
+            successful_downloads: int = int(session_state.report["is_downloaded"].sum())
+            failed_downloads: int = total_downloads - successful_downloads
+            sidebar.metric("Total Processed", total_downloads)
+            sidebar.metric("Successful", successful_downloads)
+            sidebar.metric("Failed", failed_downloads)
+                    
+        except Exception as e:
+            error(f"Error processing Shazam CSV: {str(e)}")
 
 with tab_report:
     if "report" in session_state:
