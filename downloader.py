4665cb6441d59df77b8cd9ec8b4757f952e107d5
ninja-con-gafas <absdarekar01@gmail.com>
2025-09-22 22:36:58 +0530

    refactor: extract concurrent download logic into helper

--- downloader.py
+++ downloader.py
@@ -29,35 +29,15 @@ def download_shazams(shazams: DataFrame) -> None:
         logger.info("Searching YouTube URLs for Shazam tracks.")
         shazams = shazams.assign(
             url=lambda x: x.apply(lambda row: search_youtube(f"{row['title']} {row['artist']} lyrics")[0], axis=1),
-            video_id=lambda x: x['url'].apply(get_video_id))
+            video_id=lambda x: x['url'].apply(get_video_id),
+            file_name=lambda x: x.apply(lambda row: f"{row['title']} {row['artist']} {row['video_id']}", axis=1))
 
-        logger.info("Starting concurrent downloads of audio streams.")
-        ctx = get_script_run_ctx()
-
-        with futures.ThreadPoolExecutor() as executor:
-            tasks = [
-                executor.submit(
-                    download_audio_as_mp3,
-                    download_path=DOWNLOADS_PATH,
-                    file_name=f"{row['title']} {row['artist']} {row['video_id']}",
-                    url=row["url"]
-                ) for row in shazams.to_dict(orient="records")]
-
-            for thread in executor._threads:
-                try:
-                    add_script_run_ctx(thread, ctx)
-                except Exception as e:
-                    logger.exception(f"Failed to add_script_run_ctx to thread {thread.name}: {e}")
-
-            for future in futures.as_completed(tasks):
-                try:
-                    future.result()
-                except Exception as e:
-                    logger.exception(f"Error in download task: {e}")
+        start_concurrent_downloads(records=shazams.to_dict(orient="records"),
+                                   name_key="file_name",
+                                   url_key="url")
 
         logger.info("Marking downloaded tracks and storing report in session state.")
-        session_state.report = (
-            shazams.assign(is_downloaded=lambda x: x["video_id"].apply(is_audio_downloaded)))
+        session_state.report = (shazams.assign(is_downloaded=lambda x: x["video_id"].apply(is_audio_downloaded)))
 
         logger.info("Shazam download process completed successfully.")
     except Exception as e:
@@ -78,43 +58,20 @@ def download_youtube(urls: DataFrame) -> None:
     try:
         urls = (urls.assign(video_id=lambda x: x['url'].apply(get_video_id))
         .drop_duplicates(subset=['video_id'])
-        .assign(
-            metadata=lambda x: x['video_id'].apply(get_video_metadata),
-            name=lambda x: x.apply(
-                lambda row: re.sub(
+        .assign(metadata=lambda x: x['video_id'].apply(get_video_metadata),
+                name=lambda x: x.apply(lambda row: re.sub(
                     r'[^a-zA-Z0-9]',
                     ' ',
-                    f"{row['metadata'].get('title')} {row['metadata'].get('author_name')}")
-                            + f" {row['video_id']}",
+                    f"{row['metadata'].get('title')} {row['metadata'].get('author_name')}"
+                ) + f" {row['video_id']}",
                 axis=1)))
 
-        logger.info("Starting concurrent downloads of audio streams.")
-        ctx = get_script_run_ctx()
-
-        with futures.ThreadPoolExecutor() as executor:
-            tasks = [
-                executor.submit(
-                    download_audio_as_mp3,
-                    download_path=DOWNLOADS_PATH,
-                    file_name=row["name"],
-                    url=row["url"]
-                ) for row in urls.to_dict(orient="records")]
-
-            for thread in executor._threads:
-                try:
-                    add_script_run_ctx(thread, ctx)
-                except Exception as e:
-                    logger.exception(f"Failed to add_script_run_ctx to thread {thread.name}: {e}")
-
-            for future in futures.as_completed(tasks):
-                try:
-                    future.result()
-                except Exception as e:
-                    logger.exception(f"Error in download task: {e}")
+        start_concurrent_downloads(records=urls.to_dict(orient="records"),
+                                   name_key="name",
+                                   url_key="url")
 
         logger.info("Marking downloaded tracks and storing report in session state.")
-        session_state.report = (
-            urls.assign(is_downloaded=lambda x: x["video_id"].apply(is_audio_downloaded)))
+        session_state.report = (urls.assign(is_downloaded=lambda x: x["video_id"].apply(is_audio_downloaded)))
 
         logger.info("YouTube download process completed successfully.")
     except Exception as e:
@@ -139,10 +96,10 @@ def extract_shazams(file_path: str) -> DataFrame:
     logger.info(f"Extracting Shazam data from: {file_path}")
 
     try:
-        return read_csv(filepath_or_buffer=file_path) \
-            .drop_duplicates(subset=["artist", "title"]) \
-            .drop(columns=["date", "latitude", "longitude", "status"], errors="ignore") \
-            .sort_values(by=["artist", "title"])
+        return (read_csv(filepath_or_buffer=file_path)
+            .drop_duplicates(subset=["artist", "title"])
+            .drop(columns=["date", "latitude", "longitude", "status"], errors="ignore")
+            .sort_values(by=["artist", "title"]))
     except Exception as e:
         logger.error(f"Failed to extract Shazam data: {str(e)}")
         raise Exception(f"Failed to extract Shazam data: {str(e)}")
@@ -162,9 +119,9 @@ def extract_youtube_urls(file_path: str) -> DataFrame:
     logger.info(f"Extracting YouTube URLs from: {file_path}")
 
     try:
-        return read_csv(filepath_or_buffer=file_path) \
-            .drop_duplicates(subset=["url"]) \
-            .sort_values(by=["url"])
+        return (read_csv(filepath_or_buffer=file_path)
+            .drop_duplicates(subset=["url"])
+            .sort_values(by=["url"]))
     except Exception as e:
         logger.error(f"Failed to extract YouTube URLs: {str(e)}")
         raise Exception(f"Failed to extract YouTube URLs: {str(e)}")
@@ -192,3 +149,41 @@ def is_audio_downloaded(video_id: str) -> bool:
     except Exception as e:
         logger.error(f"Error checking audio for video_id {video_id}: {str(e)}")
         return False
+
+def start_concurrent_downloads(records: list[dict], name_key: str, url_key: str) -> None:
+    """
+    Start concurrent audio downloads for the given records.
+
+    Parameters:
+        records (list[dict]): List of records containing file names and URLs.
+        name_key (str): The key in each record that provides the file name.
+        url_key (str): The key in each record that provides the URL.
+
+    Raises:
+        Exception: If any error occurs during task submission or execution.
+    """
+
+    logger.info("Starting concurrent downloads of audio streams.")
+    ctx = get_script_run_ctx()
+
+    try:
+        with futures.ThreadPoolExecutor() as executor:
+            tasks = [executor.submit(download_audio_as_mp3,
+                                     download_path=DOWNLOADS_PATH,
+                                     file_name=row[name_key],
+                                     url=row[url_key]) for row in records]
+
+            for thread in executor._threads:
+                try:
+                    add_script_run_ctx(thread, ctx)
+                except Exception as e:
+                    logger.exception(f"Failed to add_script_run_ctx to thread {thread.name}: {e}")
+
+            for future in futures.as_completed(tasks):
+                try:
+                    future.result()
+                except Exception as e:
+                    logger.exception(f"Error in download task: {e}")
+    except Exception as e:
+        logger.error(f"Error in concurrent download execution: {str(e)}")
+        raise
